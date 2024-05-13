16956c25860f44922d0e108f0a25319ccba3957d
Michele Scala <m.scala@reply.it>
2024-05-13 14:55:31 +0200

    Use custom uuid library

--- web/src/services/utils.ts
+++ web/src/services/utils.ts
@@ -1,40 +1,5 @@
 import { RACES_TYPES } from '../interfaces/db.ts';
-
-///////////////////////////////////////////////////////////////////// TAKE
-// export async function addTake(
-//     raceId: string,
-//     timeId: string,
-//     psId: string,
-//     runnerId: string,
-//     type: TakeType
-// ): Promise<Take> {
-//     const race = getRaceDB(raceId);
-//     const _id = createKey('take!', psId + '-' + runnerId + '-' + type);
-//     const found = await race.get(_id);
-//     if (found) {
-//         throw `Take ${_id} already exist`;
-//     }
-
-//     // get time
-//     const time = await timesDB.get(timeId);
-//     const take: Take = {
-//         _id: _id,
-//         time: time.time,
-//         ps: psId,
-//         runner: runnerId,
-//         type: type,
-//     };
-
-//     const resp = await race.put(take);
-//     return { ...take, _rev: resp.rev } as Take;
-// }
-
-// export async function removeTake(raceId: string, _id: string) {
-//     const race = getRaceDB(raceId);
-//     const take = await race.get<Take>(_id);
-//     await addTime(take.time);
-//     await race.remove(take);
-// }
+import { v4 as uuidv4 } from 'uuid';
 
 ////////////////////////////////////////////////////// UTILS
 export const createKey = (type: string, value: string) => {
@@ -55,7 +20,7 @@ export const getMachineId = () => {
     let machineId = localStorage.getItem('MachineId');
 
     if (!machineId) {
-        machineId = crypto.randomUUID();
+        machineId = uuidv4();
         localStorage.setItem('MachineId', machineId);
     }
     return machineId;
